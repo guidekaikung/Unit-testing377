84dc97f82da30ec4fa32dcbb9d94288e7c7bf6c9
Ratthavich Uthaijan <ratthavichuthaijan@gmai..com>
2023-10-07 18:08:48 +0700

    first commit

---
+++ src/pages/InputPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <form>
+      <input type="text" id="ชานนท์" v-model="firstName" />
+      <input type="text" id="ชัยวงค์ (6504101319)" v-model="secondName" />
+      <button @click="submitForm()" class="submitForm">Submit</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputPage',
+  data() {
+    return {
+      title: 'Vue is awesome.',
+      firstName: '',
+      secondName: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('submit form')
+    },
+  }
+}
+</script>
+
+<style scoped></style>
